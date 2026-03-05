b8009c4d7c89bf83326a0092c3a79908ab4165d1
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-03-05 20:59:30 +0530

    Update day_6patterns.py

--- day_6patterns.py
+++ day_6patterns.py
@@ -7,4 +7,56 @@
 #-----------------------------------------
 
 
-#1.
+#1.Square Star
+
+n=4
+for i in range(n):
+   for j in range(n):
+      print("*",end=" ")
+   print()
+
+#-----------------------------------------
+
+
+#2.Right Triangle 
+
+n=5
+for i in range(1,n+1):
+   for j in range(i):
+       print("*",end=" ")
+   print()
+
+#-----------------------------------------
+
+
+#3.Inverted Triangle 
+
+n=5
+for i in range(n,0,-1):
+   for j in range(i):
+       print("*",end=" ")
+   print()
+
+#-----------------------------------------
+
+
+#4.Number Triangle 
+
+n=4
+for i in range(1,n+1):
+   for j in range(1,i+1):
+       print(j,end=" ")
+   print()
+
+#-----------------------------------------
+
+
+#5.Same Number Pattern
+
+n=5
+for i in range(1,n+1):
+   for j in range(i):
+       print(i,end=" ")
+   print()
+
+#----------------------------------------

99b6fcc6fca89bd36ff65e66fdabd018e68e0257
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-03-27 08:45:47 +0530

    Update day_6patterns.py

--- day_6patterns.py
+++ day_6patterns.py
@@ -60,3 +60,37 @@ for i in range(1,n+1):
    print()
 
 #----------------------------------------
+
+
+#6.Pyramid Pattern
+
+n = 4
+
+for i in range(1,n+1):
+  print(" " * (n - i), end="")
+  for j in range(i):
+      print("*", end=" ")
+   print() 
+
+#----------------------------------------
+
+
+#7.Diamond Pattern
+
+n=5
+for i in range(1,n+1):
+   print(" "*(n-i),end="")
+   for j in range(i):
+        print("*",end=" ")
+   print()
+    
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+#--------------------------------------- 
+
+
+#8. 

ccbab888ae78ef87fda4703c4c6d980663672bcc
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-04-20 13:24:19 +0530

    Update day_7strings.py

--- day_7strings.py
+++ day_7strings.py
@@ -1,19 +1,14 @@
-#-------------------------------------------
+#-----------------------------------------
 
-# Day7_Strings
+# day_07String
 
-#-------------------------------------------
-
-
-#-------------------------------------------
+#-----------------------------------------
 
 
 #1.Accessing of strings
 
-#-------------------------------------------
-
 s="GOOD"
-print("Accessing the characters of the string:)
+print("Accessing the characters of the string:")
 
 print("Positive Indexing:")
 print("First character:",s[0])
@@ -22,18 +17,39 @@ print("Third character:",s[2])
 print("Fourth character:",s[3])
 
 print("Negative Indexing:")
-print(Fourth character:",s[-1])
-print(Third character:",s[-2])
-print(Second character:",s[-3])
-print(First character:",s[-4])
+print("Fourth character:",s[-1])
+print("Third character:",s[-2])
+print("Second character:",s[-3])
+print("First character:",s[-4])
 
 #-----------------------------------------
 
 
+#2.Slicing 
 
-#2.Reversing using loop 
+s="WELCOME"
+print("Slicing:")
+print(s[2:5:1])
+print(s[0:4:2])
+print(s[1:6])
+print(s[:5])
+print(s[4:])
 
-#-------------------------------------------
+#-----------------------------------------
+
+
+#3.Stride
+
+s="FANTASY"
+print("String after striding:")
+print(s[::3])
+print(s[::2])
+print(s[::5])
+
+#----------------------------------------
+
+
+#4.Reversing using loop
 
 org_str="WELCOME"
 a=len(org_str)
@@ -43,6 +59,15 @@ for i in range(a-1,-1,-1):
     rev_str = org_str[i]
     print( rev_str,    end=" ")
 
-#-------------------------------------------
+#-----------------------------------------
 
-#2.
+
+#5.Reversing using Stride operator 
+
+
+s="PYTHON"
+rev=s[::-1]
+print("Reverse of the sring:", rev)
+
+#----------------------------------------
+

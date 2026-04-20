e3ed3738de75153850185f71f60af1c33ee5a2c6
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-04-20 12:41:30 +0530

    Create day_7strings.py

---
+++ day_7strings.py
@@ -0,0 +1,24 @@
+#-------------------------------------------
+
+# Day7_Strings
+
+#-------------------------------------------
+
+
+#-------------------------------------------
+
+#1.Reversing 
+
+#-------------------------------------------
+
+org_str="WELCOME"
+a=len(org_str)
+print("The original String is", org_str)
+print("The reverse of the given String is", )
+for i in range(a-1,-1,-1):
+    rev_str = org_str[i]
+    print( rev_str,    end=" ")
+
+#-------------------------------------------
+
+#2.

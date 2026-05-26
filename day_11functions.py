46457f9244da73ebbd9428f9a9cee9cccdbe7933
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-05-26 20:56:53 +0530

    Create day_11functions.py

---
+++ day_11functions.py
@@ -0,0 +1,26 @@
+#1.Create and call functions 
+
+def wish():
+    print("Welcome to Python")
+
+wish()
+
+#2.Functions with paramaters 
+
+def greet(name):
+   print("Welcome," name)
+
+greet("Ajith")
+
+#3.Function with return value
+
+def add(a,b):
+  return a+b 
+
+res= add(11,22)
+
+print("Sum of two numbers is",res)
+
+#4. 
+
+  

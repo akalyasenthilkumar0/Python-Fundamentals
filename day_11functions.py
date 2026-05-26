16d33c7493e2e5e9a3622c2e77d1bd13061274de
Akalya SenthilKumar <akalyaa.027@gmail.com>
2026-05-26 21:31:25 +0530

    Update day_11functions.py

--- day_11functions.py
+++ day_11functions.py
@@ -21,6 +21,85 @@ res= add(11,22)
 
 print("Sum of two numbers is",res)
 
-#4. 
+
+#4. even/odd using function 
+
+num=int(input("Enter a number:"))
+
+def even_odd(num):
+   if num%2==0:
+     return("Even number")
+   else:
+     return("Odd number")
+
+res= even_odd(num)
+print(res)
+
+#5.Factorial using function 
+
+num = int(input("Enter a number: "))
+
+def fact(num):
+    fact = 1
+
+    if num <= 1:
+        return 1
+
+    for i in range(1, num+1):
+        fact *= i
+
+    return fact
+
+res = fact(num)
+
+print("Factorial of the given number is:", res)
+
+#6. Prime check 
+
+def prime(num):
+
+    if num <= 1:
+        return "Not Prime"
+
+    for i in range(2, num):
+        if num % i == 0:
+            return ("Not Prime")
+
+        else:
+            return ("Prime Number")
+
+result = prime(78)
+
+print(result)
+
+
+#7. lambda function
+
+add = lambda a, b: a + b
+result=add(10, 20)
+print("Sum:",result)
+
+#8. square of a number 
+
+sq=lambda t:t*t
+res=sq(56)
+print("Square of 56:",res)
+
+#9.Recursive function
+
+num=int(input("Enter a number:"))
+
+def factorial(num):
+    if num==1:
+        return 1
+    else:
+        return num*factorial(num - 1)
+
+result=factorial(num)
+
+print("Factorial=",result)
+
+
+
 
   
